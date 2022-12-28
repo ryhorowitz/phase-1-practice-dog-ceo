2b177f9bdcaa73f1a1941effd842236bc96186e9
ryhorowitz <ryan.w.horowitz@gmail.com>
2022-12-28 17:16:55 -0500

    Challenge 4 complete

--- src/index.js
+++ src/index.js
@@ -3,9 +3,33 @@ const breedList = document.querySelector('#dog-breeds')
 const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
 const breedUrl = 'https://dog.ceo/api/breeds/list/all'
 const imgDiv = document.createElement('div')
-
+const firstLetterSelector = document.querySelector("#breed-dropdown")
+console.log(firstLetterSelector)
 function changeColor() {
   this.style.color = "red";
+}
+
+function filterBreeds(e) {
+  // create selector for select tag
+  // console.log(firstLetterSelector)
+  const letter = e.target.value
+  console.log(letter)
+
+  const filteredBreedListLIs = Array.from(breedList.children).filter( breedLI => {
+    if (letter === breedLI.id[0]) {
+      return breedLI
+    }
+  })
+  console.log(filteredBreedListLIs)
+  //remove all children of the ul
+  while (breedList.firstChild) {
+    breedList.removeChild(breedList.firstChild)
+  }
+  filteredBreedListLIs.forEach( breed => {
+    breedList.appendChild(breed)
+  })
+  //replace with li from filteredBreedList
+
 }
 
 document.addEventListener('DOMContentLoaded', () => {
@@ -31,15 +55,11 @@ document.addEventListener('DOMContentLoaded', () => {
   const getBreeds = fetch(breedUrl)
     .then(res => res.json())
     .then(data => {
-      // console.log(data.message)
       const breeds = data.message
-      // adds the breeds to the page in the <ul> provided in index.html
-      //foreach breed create a li and appendChild to the breedsList
       for (let breed in breeds) {
         const breedLI = document.createElement('li')
         breedLI.id = breed
         breedLI.textContent = breed
-        //onClick change color
         breedLI.addEventListener('click', changeColor)
         breedList.appendChild(breedLI)
       }
@@ -47,6 +67,5 @@ document.addEventListener('DOMContentLoaded', () => {
     })
     .catch(err => console.error(err.message))
 
-    // Once all of the breeds are rendered in the <ul>, add JavaScript so that, when the user clicks on any one of the <li>s, the font color of that <li> changes. This can be a color of your choosing.
-
+    firstLetterSelector.addEventListener('change', filterBreeds)
 })

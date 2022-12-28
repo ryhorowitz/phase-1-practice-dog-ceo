6669ccc22e1bfa7e01479f75334fa900f078a719
ryhorowitz <ryan.w.horowitz@gmail.com>
2022-12-28 16:13:35 -0500

    Challenge 1 complete

--- src/index.js
+++ src/index.js
@@ -1,21 +1,27 @@
 console.log('%c HI', 'color: firebrick')
-const imgContainer = document.querySelector('#dog-image-container')
+const div = document.querySelector('.my-class')
 const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
 const breedUrl = 'https://dog.ceo/api/breeds/list/all'
-
+const imgDiv = document.createElement('div')
 
 document.addEventListener('DOMContentLoaded', () => {
-  const imgTag = document.createElement('img')
-
+  
   const dogImgs = fetch(imgUrl)
-    .then( res => res.json())
-    .then( data => {
-      console.log(data.message)
-      //iterate through each img and append to the dom
-
+  .then( res => res.json())
+  .then( data => {
+    console.log(data.message)
+    const pics = data.message
+    //iterate through each img and append to the dom
+    pics.forEach( (pic, index) => {
+      const imgTag = document.createElement('img')
+      imgTag.id = 'dog-' + index
+      imgTag.src = pic
+      imgTag.alt = 'Doggie Picture'
+      console.log(imgTag)
+      console.log(div) // returning null????
+      div.appendChild(imgTag)
+      });
     })
     .catch( err => console.error(err.message))
-  dogImgs.alt = 'Doggie Pic'
-  console.log(dogImgs)
-  imgContainer.appendChild(dogImgs)
+
 })
